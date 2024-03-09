99bfbdf13ba18eaa8048a8ff63a08b50759bae3a
mohamedpasyony <mohamedpasyony41@gmail.com>
2024-03-09 23:41:48 +0200

    Create Admin Layout

--- Bookify/Program.cs
+++ Bookify/Program.cs
@@ -28,7 +28,7 @@ namespace Bookify
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Customer}/{action=Index}/{id?}");
 
             app.Run();
         }
