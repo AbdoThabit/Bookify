38619aeb825cd1e6226f699ed21c21d56c349446
Abdelrahman Thabit <abdelrahman4ever98@gmail.com>
2024-03-13 15:11:42 +0200

    Adding roomservice and the roomviewmodel

---
+++ Bookify/RoomRepositary/IRoomRepo.cs
@@ -0,0 +1,14 @@
+﻿using Bookify.Models;
+using Bookify.ViewModel;
+
+namespace Bookify.RoomRepositary
+{
+    public interface IRoomRepo
+    {
+        public List<Room> getAllRooms();
+        public  List<RoomType> getAllRoomsTypes();
+        public Room Add(RoomVM model);
+        public Room delete(Room room);
+        public Room Update(Room room);
+    }
+}
