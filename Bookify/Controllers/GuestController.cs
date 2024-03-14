0e839116a176966e967593bbb15412047026f571
Abdelrahman Thabit <abdelrahman4ever98@gmail.com>
2024-03-14 13:02:35 +0200

    done fixing

--- Bookify/Controllers/GuestController.cs
+++ Bookify/Controllers/GuestController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Bookify.Models;
+using Bookify.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Bookify.Controllers
 {
-    [Authorize(Roles = "Admin")]
-
     public class GuestController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        GuestController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [Authorize(Roles = "Admin")]
+
         public IActionResult Index()
         {
             return View();
